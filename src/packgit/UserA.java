28625907b7cb40fb9ca0a3148fb94296f7d7463f
Ashwini1Ban <ashwini93@live.com>
2024-12-24 11:15:26 +0530

    my first commit

---
+++ src/packgit/UserA.java
@@ -0,0 +1,27 @@
+package packgit;
+
+public class UserA {
+
+	
+	void t1()
+	{
+		System.out.println("hii");
+	}
+
+	
+	void t2() 
+	{
+		System.out.println("hello");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		UserA aa=new UserA();
+		aa.t1();
+		aa.t2();
+		
+	}
+
+}
