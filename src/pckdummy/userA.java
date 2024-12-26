bbde215e60e641e63ff0cc9c29c46d233d14b1b0
Ashwini1Ban <ashwini93@live.com>
2024-12-26 15:06:28 +0530

    first commit

---
+++ src/pckdummy/userA.java
@@ -0,0 +1,23 @@
+package pckdummy;
+
+public class userA {
+
+	void t1()
+	{
+		System.out.println("hi");
+	}
+	
+	void t2()
+	{ 
+		System.out.println("hello");
+	}
+	
+	
+	public static void main(String[] args) {
+		userA aa=new userA();
+	    aa.t1();
+		aa.t2();
+		
+	}
+
+}
